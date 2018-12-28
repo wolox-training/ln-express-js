6eae9e39b7e57bfd5efb3158c8f54753ac160696
Lucas Nevado <nevadolucas@gmail.com>
2018-12-28 11:58:12 -0300

    Added login tests

--- test/users.js
+++ test/users.js
@@ -5,11 +5,19 @@ const chai = require('chai'),
   assert = require('chai').assert,
   should = chai.should(),
   bcrypt = require('bcryptjs'),
-  User = require('../app/models').user;
+  User = require('../app/models').user,
+  sessionManager = require('./../app/services/sessionManager');
+
+const successfulLogin = () => {
+  return chai
+    .request(server)
+    .post('/users/sessions')
+    .send({ email: 'email1@wolox.com.ar', password: 'wolox1189' });
+};
 
 chai.use(chaiHttp);
 describe('/users POST', () => {
-  it('should fail because email is missing', done => {
+  it('registration should fail because email is missing', done => {
     const userWithoutEmail = {
       firstName: 'John',
       lastName: 'Smith',
@@ -27,7 +35,7 @@ describe('/users POST', () => {
       })
       .then(() => done());
   });
-  it('should fail because password does not meet requirements', done => {
+  it('registration should fail because password does not meet requirements', done => {
     const userWithPasswordNotMeetingRequirements = {
       firstName: 'John',
       lastName: 'Smith',
@@ -46,7 +54,7 @@ describe('/users POST', () => {
       })
       .then(() => done());
   });
-  it('should be succesful', done => {
+  it('registration should be succesful', done => {
     const sampleUser = {
       firstName: 'John',
       lastName: 'Smith',
@@ -93,6 +101,84 @@ describe('/users POST', () => {
         err.response.should.be.json;
         err.response.body.should.have.property('message');
         err.response.body.should.have.property('internal_code', 'invalid_email');
+      })
+      .then(() => done());
+  });
+});
+
+describe('/user/sessions POST', () => {
+  it('login should be a succesful', done => {
+    const sampleUser = { email: 'email1@wolox.com.ar', password: 'wolox1189' };
+    chai
+      .request(server)
+      .post('/users/sessions')
+      .send(sampleUser)
+      .then(res => {
+        res.should.have.status(200);
+        res.body.should.have.property('firstName');
+        res.body.should.have.property('lastName');
+        res.body.should.have.property('email');
+        res.body.should.have.property('password');
+        res.headers.should.have.property(sessionManager.HEADER_NAME);
+        const resEmail = sessionManager.decode(res.headers.authorization).email;
+        assert(sampleUser.email === resEmail);
+        dictum.chai(res);
+      })
+      .then(() => done());
+  });
+  it('login should fail because of invalid username', done => {
+    const invalidUserEmail = { email: 'invalidemail@wolox.com.ar', password: 'wolox1189' };
+    chai
+      .request(server)
+      .post('/users/sessions')
+      .send(invalidUserEmail)
+      .catch(err => {
+        err.should.have.status(400);
+        err.response.should.be.json;
+        err.response.body.should.have.property('message');
+        err.response.body.should.have.property('internal_code', 'invalid_user');
+      })
+      .then(() => done());
+  });
+  it('login should fail because of invalid password', done => {
+    const invalidUserPassword = { email: 'email1@wolox.com.ar', password: 'invalidPassword' };
+    chai
+      .request(server)
+      .post('/users/sessions')
+      .send(invalidUserPassword)
+      .catch(err => {
+        err.should.have.status(400);
+        err.response.should.be.json;
+        err.response.body.should.have.property('message');
+        err.response.body.should.have.property('internal_code', 'invalid_user');
+      })
+      .then(() => done());
+  });
+  it('login should fail because of missing password', done => {
+    const invalidUserNoPassword = { email: 'email1@wolox.com.ar' };
+    chai
+      .request(server)
+      .post('/users/sessions')
+      .send(invalidUserNoPassword)
+      .catch(err => {
+        err.should.have.status(400);
+        err.response.should.be.json;
+        err.response.body.should.have.property('message');
+        err.response.body.should.have.property('internal_code', 'missing_parameters');
+      })
+      .then(() => done());
+  });
+  it('login should fail because of missing email', done => {
+    const invalidUserNoEmail = { password: 'invalidPassword' };
+    chai
+      .request(server)
+      .post('/users/sessions')
+      .send(invalidUserNoEmail)
+      .catch(err => {
+        err.should.have.status(400);
+        err.response.should.be.json;
+        err.response.body.should.have.property('message');
+        err.response.body.should.have.property('internal_code', 'missing_parameters');
       })
       .then(() => done());
   });
