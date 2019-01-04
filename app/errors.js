8db2fcff2948b696658c340d512d5dc95e4eb7d6
Lucas Nevado <nevadolucas@gmail.com>
2019-01-04 17:13:20 -0300

    Removed example errors an added use strict

--- app/errors.js
+++ app/errors.js
@@ -12,12 +12,6 @@ exports.invalidEmail = internalError(
 exports.ENCRYPTION_ERROR = 'encryption_error';
 exports.encryptionError = message => internalError(message, exports.ENCRYPTION_ERROR);
 
-exports.INVALID_USER = 'invalid_user';
-exports.invalidUser = internalError('Invalid username or password', exports.INVALID_USER);
-
-exports.BOOK_NOT_FOUND = 'book_not_found';
-exports.bookNotFound = internalError('Book not found', exports.BOOK_NOT_FOUND);
-
 exports.SAVING_ERROR = 'saving_error';
 exports.savingError = message => internalError(message, exports.SAVING_ERROR);
 
--- app/middlewares/errors.js
+++ app/middlewares/errors.js
@@ -6,8 +6,6 @@ const DEFAULT_STATUS_CODE = 500;
 const statusCodes = {
   [errors.INVALID_EMAIL]: 400,
   [errors.ENCRYPTION_ERROR]: 500,
-  [errors.INVALID_USER]: 400,
-  [errors.BOOK_NOT_FOUND]: 404,
   [errors.SAVING_ERROR]: 400,
   [errors.DATABASE_ERROR]: 503,
   [errors.DEFAULT_ERROR]: 500
